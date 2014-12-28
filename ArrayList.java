5b5f24b5f2e5bbe7a32ead2f9dc6e4dbd03285f6
John <jspears623@gmail.com>
2014-12-28 16:31:16 +0000

    made a lot of changes to arrayList

--- ArrayList.java
+++ ArrayList.java
@@ -1,16 +1,17 @@
 public class ArrayList implements List {
-	int numberList;
+	private Object[] objList;
 	public ArrayList(int length) {
-		this.numberList = new int[length];
+		this.objList = new Object[length];
 	}
+	
 	/**
 	 * Returns true if the list is empty, false otherwise. 
 	 * 
 	 * @return true if the list is empty, false otherwise. 
 	 */
 	public boolean isEmpty(
-		for(int i = 0; i < this.numberList.length; i++) {
-			if (this.numberList != null) {
+		for(int i = 0; i < this.objList.length; i++) {
+			if (this.objList != null) {
 				return true;
 			}
 		}
@@ -25,14 +26,36 @@ public class ArrayList implements List {
 	 */
 	public int size(
 		int itemCount = 0;
-		for(int i = 0; i < this.numberList.length; i++) {
-			if (this.numberList != null) {
+		for(int i = 0; i < this.objList.length; i++) {
+			if (this.objList != null) {
 				itemCount++;
 			}
 		}
 		return itemCount;
 	);
-
+	/** 
+	*	Checks whether an item is out of bounds of the array
+	*	Saves us writing this out a few times
+	*	@param index The position in the array that we are trying to access
+	*  	@return a ReturnObject with or without errors	
+	*/
+	private ReturnObject errorCheck(int index) {
+		ReturnObject result = new ReturnObjectImpl();
+		if ((index > this.objList.length) || (index < 0)) {
+			//It's out of bounds
+			result.setError = true;
+			result.setErrorDetails((ErrorMessage) INDEX_OUT_OF_BOUNDS);
+		} else if (index = null) {
+			//The argument is null - we need to stop that
+			result.setError = true;
+			result.setErrorDetails((ErrorMessage) INVALID_ARGUMENT);
+		} else if (this.objList(index) == null) {
+			//There is nothing there!
+			result.setError = true;
+			result.setErrorDetails((ErrorMessage) EMPTY_STRUCTURE);
+		}
+		return result;
+	}
 	/**
 	 * Returns the elements at the given position. 
 	 * 
@@ -43,11 +66,17 @@ public class ArrayList implements List {
 	 * @return the element or an appropriate error message, 
 	 *         encapsulated in a ReturnObject
 	 */
+	 
 	public ReturnObject get(int index) {
-		ReturnObject result = new ReturnObjectImpl();
-		if (index > this.numberList.length) {
-
+		//First check for any errors
+		ReturnObject result = errorCheck(index);
+		
+		if (result.hasError == false) {
+			//Get the item from the array
+			result.setItem(objList[index]);
 		}
+		
+		return result;
 	};
 
 	/**
@@ -62,7 +91,14 @@ public class ArrayList implements List {
 	 * @return the element or an appropriate error message, 
 	 *         encapsulated in a ReturnObject
 	 */
-	public ReturnObject remove(int index);
+	public ReturnObject remove(int index) {
+		ReturnObject result = errorCheck(index);
+		if (result.hasError == false) {
+			result.setItem(objList[index]);
+			objList[index] = null;
+		}
+		return result;
+	};
 
 	/**
 	 * Adds an element to the list, inserting it at the given
@@ -82,7 +118,15 @@ public class ArrayList implements List {
 	 * @return an ReturnObject, empty if the operation is successful
 	 *         the item added or containing an appropriate error message
 	 */
-	public ReturnObject add(int index, Object item);
+	public ReturnObject add(int index, Object item) {
+		ReturnObject result = errorCheck(index);
+		if (this.objList[index] == null) {
+			this.objList[index] = item;
+		} else {
+			redimList(index, item);		
+		}
+		return result;
+	};
 
 	/**
 	 * Adds an element at the end of the list.
@@ -95,6 +139,27 @@ public class ArrayList implements List {
 	 * @return an ReturnObject, empty if the operation is successful
 	 *         the item added or containing an appropriate error message
 	 */
-	public ReturnObject add(Object item);
+	public ReturnObject add(Object item) {
+		ReturnObject result = errorCheck(index);
+		redimList(this.objList.length, item);
+		return result;	
+	};
+	/**
+	* Sometimes we are going to need to redim an the list array if we are inserting more objects in it
+	* 
+	*/
+	private void redimList(int index, Object item) {
+		Object[] tmpList = new Object[this.objList.length + 1];
+		for(int i = 0; i < tmpList.length; i++) {
+			if (i < index) {
+				tmpList[i] = this.objList[i];
+			} else if (i == index) {
+				tmpList[i] = item;
+			} else {
+				tmpList[i] = this.objList[i-1];
+			}
+		}
+		this.objList = tmpList;
+	}
 
 }
--- ImprovedStackImpl.java
+++ ImprovedStackImpl.java
@@ -1,2 +1,3 @@
 public class ImprovedStackImpl implements ImprovedStack{
+	
 }
