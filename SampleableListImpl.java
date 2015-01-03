f486522c9570adb5e09a3acc5694525d751dd9b1
John <jspears623@gmail.com>
2015-01-03 13:04:25 +0000

    First implementation of SampleableList and SampleableListImpl

--- SampleableListImpl.java
+++ SampleableListImpl.java
@@ -1,16 +1,15 @@
-public class SampleableListImpl implements SampleableList {
-
-	public ReturnObjectImpl() {
-	}
-	
-	
-	public boolean hasError() {
-	}
-	
-	public ErrorMessage getError() {
-	}
-	
-	public Object getReturnValue() {
-	}
+public class SampleableListImpl extends LinkedList implements SampleableList {
+	//using linked list rather than array list as the array list size issue causes confusion
+	public SampleableList sample(){
+		SampleableList sampledList = new SampleableListImpl();
+		if(!(this.isEmpty())) {
+			for(int i = 0; i < this.size(); i++) {
+				if((i % 2) != 0) {
+					sampledList.add(this.get(i));
+				}
+			}
+		}
+		return sampledList;
+	};
 
 }
