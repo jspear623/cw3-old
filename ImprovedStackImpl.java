8e48f5a76880710d8d2b360bb8ed307fb37e35ae
John <jspears623@gmail.com>
2015-01-04 16:26:25 +0000

    Improved stack implemented

--- ImprovedStackImpl.java
+++ ImprovedStackImpl.java
@@ -1,3 +1,42 @@
 public class ImprovedStackImpl implements ImprovedStack{
+	LinkedList objList;
+	public ImprovedStackImpl() {
+		this.objList = new LinkedList();
+	}
 	
+	public boolean isEmpty() {
+		return this.objList.isEmpty();
+	}
+	
+	public int size() {
+		return this.objList.size();
+	}
+	
+	public void push(Object item) {
+		this.objList.add(0,item);
+	}
+	
+	public ReturnObject pop() {
+		return this.objList.remove(0);
+	}
+	
+	public ReturnObject top() {
+		return this.objList.get(0);
+	}
+	
+	public ImprovedStack reverse() {
+		ImprovedStackImpl reverseStack = new ImprovedStackImpl();
+		for (int i = 0; i < this.size(); i++) {
+			reverseStack.push(this.objList.get(i).getReturnValue());
+		}
+		return reverseStack;
+	}
+	
+	public void remove(Object item) {
+		for (int i = 0; i < this.size(); i++) {
+			if (this.objList.get(i).getReturnValue().equals(item)) {
+				this.objList.remove(i);
+			}
+		}
+	}	
 }
